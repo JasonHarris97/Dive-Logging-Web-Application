8c7270195cfbcc1af5d72250ad36d48683cde4f9
Jason Harris <jason.harris-2@student.manchester.ac.uk>
2020-02-18 19:30:12 +0000

    Added Role.java model
    Added data model for role-based security

--- DiveLoggingWebApp/src/main/java/app/config/SecurityConfiguration.java
+++ DiveLoggingWebApp/src/main/java/app/config/SecurityConfiguration.java
@@ -1,20 +1,25 @@
-
-
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import app.services.UserService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+        	.authorizeRequests()
             .antMatchers("/registration").permitAll()
         	.antMatchers("/confirm").permitAll()
         	.anyRequest().authenticated()
