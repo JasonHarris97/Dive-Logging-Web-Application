d5ef78991eebe5f7221483c2061ef8bc3d28a371
Jason Harris <jason.harris-2@student.manchester.ac.uk>
2020-02-18 20:53:56 +0000

    Updated User.java
    New User.java model stucture to work with new registration
    implementation

--- DiveLoggingWebApp/src/main/java/app/web/UserDto.java
+++ DiveLoggingWebApp/src/main/java/app/web/UserDto.java
@@ -4,6 +4,8 @@ import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
+import app.constraint.FieldMatch;
+
 @FieldMatch.List({
     @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match"),
     @FieldMatch(first = "email", second = "confirmEmail", message = "The email fields must match")
