def8fd1f10f8d4e5852b49494cc6e5ff38216231
Jason Harris <jason.harris-2@student.manchester.ac.uk>
2020-02-18 20:14:51 +0000

    Added MainController, index and login templates, updates
    Added main controller to for navigating to various pages.
    Added index and login html templates. These need modifying.
    Updated application.properties to create tables and added some
    dependencies to pom.xml
    Also updated added Roles to User model

--- DiveLoggingWebApp/src/main/java/app/models/User.java
+++ DiveLoggingWebApp/src/main/java/app/models/User.java
@@ -2,12 +2,21 @@
 
 package app.models;
 
+import java.util.Collection;
+
+/*import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;*/
+
+import javax.persistence.*;
+
 
 import org.springframework.data.annotation.Transient;
 
@@ -15,12 +24,16 @@ import org.springframework.data.annotation.Transient;
 @Table(name = "user")
 public class User {
 	
-	// USER ATTRIBUTES
-	// ---------------
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
 	
 	@Column(name = "email", nullable = false, unique = true)
 	private String email;
@@ -28,12 +41,6 @@ public class User {
 	@Column(name = "password")
 	@Transient
 	private String password;
-	
-	@Column(name = "first_name")
-	private String firstName;
-	
-	@Column(name = "last_name")
-	private String lastName;
 	
 	@Column(name = "username")
 	private String username;
@@ -44,6 +51,32 @@ public class User {
 	@Column(name = "confirmation_token")
 	private String confirmationToken;
 	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	   @JoinTable(
+	        name = "users_roles",
+	        joinColumns = @JoinColumn(
+	            name = "user_id", referencedColumnName = "id"),
+	        inverseJoinColumns = @JoinColumn(
+	            name = "role_id", referencedColumnName = "id"))
+	private Collection<Role> roles;
+	
+	// Constructors
+	public User() {}
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password, Collection < Role > roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
 	
 	// Getters and Setters 
 	// -------------------
@@ -54,6 +87,7 @@ public class User {
 	public void setConfirmationToken(String confirmationToken) {
 		this.confirmationToken = confirmationToken;
 	}
+	
 
 	public int getId() {
 		return id;
@@ -62,15 +96,8 @@ public class User {
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
+	
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -78,6 +105,7 @@ public class User {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 
 	public String getLastName() {
 		return lastName;
@@ -87,6 +115,34 @@ public class User {
 		this.lastName = lastName;
 	}
 	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	public Collection < Role > getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection < Role > roles) {
+        this.roles = roles;
+    }
+    
+	
 	public String getUsername() {
 		return username;
 	}
@@ -95,13 +151,6 @@ public class User {
 		this.username = username;
 	}
 
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 	public boolean getEnabled() {
 		return enabled;
@@ -110,4 +159,17 @@ public class User {
 	public void setEnabled(boolean value) {
 		this.enabled = value;
 	}
+	
+	
+	@Override
+    public String toString() {
+        return "User{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", email='" + email + '\'' +
+            ", password='" + "*********" + '\'' +
+            ", roles=" + roles +
+            '}';
+    }
 }
