72ddf2432209d6511494bf264a20c4ac03fbd432
Jason Harris <jason.harris-2@student.manchester.ac.uk>
2020-03-09 11:55:53 +0000

    Added more test data
    Added more test users and dives

--- DiveLoggingWebApp/src/main/java/app/config/InitialDataLoader.java
+++ DiveLoggingWebApp/src/main/java/app/config/InitialDataLoader.java
@@ -43,9 +43,9 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 	private final static Random rand = new Random();
 	private final static StringLists stringLists = new StringLists();
 	
-	private final static int noOfTestUsers = 10;
+	private final static int noOfTestUsers = 20;
 	
-	private final static int noOfTestDives = 10;
+	private final static int noOfTestDives = 1000;
 	
 	@Autowired
 	private UserService userService;
